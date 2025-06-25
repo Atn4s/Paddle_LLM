a56247ba03ea05e03ca041d078a411ac3a6baaf2
João Paulo <61942303+Atn4s@users.noreply.github.com>
2025-06-25 18:15:04 -0300

    v1 - Add pipeline (JP_Paddle_OCR e LLM_Gladiator)
    Adicionado script para orquestrar a instalação de ambos os projetos

---
+++ pipeline.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Script para instalar os projetos JP_Paddle_OCR e LLM_Gladiator hospedado no github
+# Autor: Atn4s com ajuda da IA 👾
+
+LLM_PATH="LLM_Gladiator/LLM_Gladiator"
+OCR_PATH="JP_Paddle_OCR/PaddleOCR"
+
+# Menu_PaddleOCR+LLM_Gladiator
+menu_usuario() {
+    clear
+    echo "[PaddleOCR + LLM_Gladiator]"
+    echo "======================="
+    echo "1. Instalar PaddleOCR e LLM_Gladiator"
+    echo "2. Executar ambos projetos"
+    echo "3. Sair"
+    echo "======================="
+}
+
+instalacao_Paddle_LLM(){
+    git clone https://github.com/Atn4s/JP_Paddle_OCR || { echo "Falha no git clone"; exit 1; }
+    git clone https://github.com/Atn4s/LLM_Gladiator || { echo "Falha no git clone"; exit 1; }
+    cd JP_Paddle_OCR/
+    bash install.sh
+    cd ..
+    sleep 10
+    clear
+    cd LLM_Gladiator/
+    bash install.sh
+    cd ..
+    sleep 10
+    clear
+    echo "A instalação do PaddleOCR e LLM_Gladiator foi realizada!"
+    echo "Para executar a opção 2 por favor verefique se:"
+    echo "Inseriu as imagens no diretório: JP_Paddle__OCR/PaddleOCR/OCRDatabase"
+    echo "Inseriu as imagens no diretório: LLM_Gladiator/LLM_Gladiator/images"
+    echo "Coloque seu arquivo .env no diretório: LLM_Gladiator/LLM_Gladiator"
+    sleep 10
+}
+
+
+executar_modelos(){
+    pushd "$OCR_PATH"
+    if [ -f "bin/activate" ]; then
+        source bin/activate
+    else
+        echo "Ambiente virtual não encontrado em $PWD"
+        return 1
+    fi
+    bash processa_imagens.sh
+    echo "O Paddle processou as imagens, elas estão salvas em:"
+    echo "Imagens + .txt: PaddleOCR/Resultados_OCR"
+    echo "JSON: PaddleOCR/Saida_Processada"
+    deactivate
+    sleep 10
+    clear
+    popd
+    pushd "$LLM_PATH"
+    if [ -f "bin/activate" ]; then
+        source bin/activate
+    else
+        echo "Ambiente virtual não encontrado em $PWD"
+        return 1
+    fi
+    bash processa_LLM.sh
+    echo "O LLM_Gladiator processou as imagens, elas estão salvas em:"
+    echo "Resultados do LLM: LLM_Gladiator/results"
+    echo "JSON Formatado: LLM/Gladiator/resultados_limpos"
+    deactivate
+    sleep 10
+    clear
+    popd
+}
+
+while true; do
+    menu_usuario
+    read -p "Escolha uma opção (1/2/3): " escolha_usuario
+
+    case $escolha_usuario in
+        1)
+            instalacao_Paddle_LLM
+            ;;
+        2)
+            executar_modelos
+            ;;
+        3)
+            echo "Saindo do programa."
+            sleep 1
+            clear
+            break
+            ;;
+        *)
+            echo "Opcao Invalida, tente novamente."
+            ;;
+    esac
+done
